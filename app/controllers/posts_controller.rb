c9f32f726ef084cbbaccaef1374abb497b8f1ee5
mhfairbank <mhfairbank@gmail.com>
2015-01-31 20:04:01 -0500

    finish scraper

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,6 @@ class PostsController < ApplicationController
 
 	private
     def post_params
-      params.require(:post).permit(:venue, :details, :address, :price, :description)
+      params.require(:post).permit(:venue, :details, :address, :price, :description, :url)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,16 @@ require 'open-uri'
 
 class Post < ActiveRecord::Base
 	def self.scrape_pulsd
+		# Without ids, scraping is based on direct children and styles
 		listings_page = Nokogiri::HTML(open("http://www.pulsd.com/new-york"))
 		listings_array = listings_page.css(".pulse-box a")
 		listings_array.each do |listing|
 			post_url = "http://www.pulsd.com" + listing.attributes["href"].value
 			unless Post.all.pluck(:url).include?(post_url)
 				post_page = Nokogiri::HTML(open(post_url))
+				post = Hash.new
+				post[:url] = post_url
 				if post_page.css("div[style='color: black; font-size: 30px; margin-top: 15px; padding-bottom: 15px; color: #48BC96; margin-left: 15px; letter-spacing: 2px;']").size > 0
-					post = Hash.new
-					post[:url] = post_url
 					post[:venue] = post_page.css(".grid_5 > div > div[style='font-size: 20px; font-weight: bold; letter-spacing: 1px;']").children.first.text[1..-2]
 					post[:description] = post_page.css(".grid_5 > div > div[style='font-size: 15px; margin-top: 20px; margin-bottom: 10px; letter-spacing: 1px;']").children.first.text[1..-2]
 					post[:address] = post_page.css(".grid_5 > .grey-background.product-text.pjsLinkToNewTab").first.children[2].text[1..-2]
@@ -19,10 +20,15 @@ class Post < ActiveRecord::Base
 					post_page.css(".grid_9 > .pjsLinkToNewTab").first.children.each_with_index do |child, index|
 						post[:details] += "#{child.text} " if index.odd?
 					end
-					Post.create(post)
 				else
-					# build this
+					post[:venue] = post_page.css(".grid_7 > div > div > div[style='float: left;']").text[1..-2]
+					post[:price] = "Free"
+					post[:description] = post_page.css(".grid_7 > div > div[style='font-size: 15px; margin-bottom: 20px;']").text[1..-2]
+					end_address_index = post_page.css(".grid_7 > div > div[style='font-size: 14px']").text[2..-1].index("\n") + 1
+					post[:address] = post_page.css(".grid_7 > div > div[style='font-size: 14px']").text[2..end_address_index]
+					post[:details] = post_page.css(".grid_14 > div > .pjsLinkToNewTab > p").text
 				end
+				Post.create(post)
 			end
 		end
 	end
