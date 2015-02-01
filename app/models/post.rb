cf820471e16bd46cc71bccfd345b696742c48c37
mhfairbank <mhfairbank@gmail.com>
2015-02-01 13:35:23 -0500

    expand functionality of scraper

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,11 +13,15 @@ class Post < ActiveRecord::Base
 				post_page = Nokogiri::HTML(open(post_url))
 				post = Hash.new
 				post[:url] = post_url
-				if post_page.css("div[style='color: black; font-size: 30px; margin-top: 15px; padding-bottom: 15px; color: #48BC96; margin-left: 15px; letter-spacing: 2px;']").size > 0
+				if post_page.css(".grid_5").size > 0
 					post[:venue] = post_page.css(".grid_5 > div > div[style='font-size: 20px; font-weight: bold; letter-spacing: 1px;']").children.first.text[1..-2]
 					post[:description] = post_page.css(".grid_5 > div > div[style='font-size: 15px; margin-top: 20px; margin-bottom: 10px; letter-spacing: 1px;']").children.first.text[1..-2]
 					post[:address] = post_page.css(".grid_5 > .grey-background.product-text.pjsLinkToNewTab").first.children[2].text[1..-2]
-					post[:price] = post_page.css("div[style='color: black; font-size: 30px; margin-top: 15px; padding-bottom: 15px; color: #48BC96; margin-left: 15px; letter-spacing: 2px;']").first.text[1..-3]
+					if post_page.css("div[style='color: black; font-size: 30px; font-weight: 200; margin-top: 15px; padding-bottom: 15px;']").size > 0
+						post[:price] = post_page.css("div[style='color: black; font-size: 30px; font-weight: 200; margin-top: 15px; padding-bottom: 15px;']").first.text[1..-3]
+					elsif post_page.css("div[style='color: black; font-size: 30px; margin-top: 15px; padding-bottom: 15px; color: #48BC96; margin-left: 15px; letter-spacing: 2px;']").size > 0
+						post[:price] = post_page.css("div[style='color: black; font-size: 30px; margin-top: 15px; padding-bottom: 15px; color: #48BC96; margin-left: 15px; letter-spacing: 2px;']").first.text[1..-3]
+					end
 					post[:details] = ""
 					post_page.css(".grid_9 > .pjsLinkToNewTab").first.children.each_with_index do |child, index|
 						post[:details] += "#{child.text} " if index.odd?
@@ -37,5 +41,3 @@ class Post < ActiveRecord::Base
 		counter
 	end
 end
-
-
