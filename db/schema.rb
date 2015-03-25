8ed048cb4f8577aab4ddb498b48b50d0ed7e4739
mhfairbank <mhfairbank@gmail.com>
2015-03-24 23:35:28 -0400

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131231420) do
+ActiveRecord::Schema.define(version: 20150325011600) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "venue"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20150131231420) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.string   "url"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email_address"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
